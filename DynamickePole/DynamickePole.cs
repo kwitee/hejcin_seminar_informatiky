7970ba7a82677da56f350277d4de1ddf6554334a
Ondřej Květ <ondrej.kvet@thermofisher.com>
2023-11-12 16:17:19 +0100

    Implementace dynamického pole

---
+++ DynamickePole/DynamickePole.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace DynamickePole
+{
+    public class DynamickePole<T>
+    {
+        private T[] vnitrniPole;
+
+        public int PocetPrvku { get; private set; }
+
+        public DynamickePole(int kapacita)
+        {
+            vnitrniPole = new T[kapacita];
+        }
+
+        // Přidá nový prvek na konec pole.
+        public void PridejPrvek(T prvek)
+        {
+            // Pokud je vnitřní pole plné, vytvoříme nové pole s dvojnásobnou kapacitou
+            // a překopírujeme do něj prvky z původního pole.
+            if (PocetPrvku >= vnitrniPole.Length)
+            {
+                T[] novePole = new T[PocetPrvku * 2];
+
+                for (int i = 0; i < PocetPrvku; i++)
+                {
+                    novePole[i] = vnitrniPole[i];
+                }
+
+                vnitrniPole = novePole;
+            }
+
+            // Přidáme prvek na konec pole a zvýšíme skutečný počet prvků.
+            vnitrniPole[PocetPrvku] = prvek;
+            PocetPrvku++;
+        }
+
+        // Vrátí prvek na daném indexu.
+        public T VratPrvek(int index)
+        {
+            if (index < 0 || index >= PocetPrvku)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return vnitrniPole[index];
+        }
+
+        // Smaže prvek na daném indexu.
+        public void VymazPrvek(int index)
+        {
+            if (index < 0 || index >= PocetPrvku)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            // Přesuneme prvky v poli o jednu pozici doleva (začneme prvkem na indexu).
+            for (int i = index; i < PocetPrvku - 1; i++)
+            {
+                vnitrniPole[i] = vnitrniPole[i + 1];
+            }
+
+            // Snížíme skutečný počet prvků.
+            PocetPrvku--;
+        }
+    }
+}
---
+++ DynamickePole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DynamickePole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vytvoříme nové pole stringů s kapacitou 3.");
+            var dynamickePole = new DynamickePole<string>(3);
+            Console.WriteLine();
+
+            Console.WriteLine("Počet prvků by měl být 0.");
+            Console.WriteLine(dynamickePole.PocetPrvku);
+            Console.WriteLine();
+
+            Console.WriteLine("Přidáme 5 prvků.");
+            dynamickePole.PridejPrvek("Prvek0");
+            dynamickePole.PridejPrvek("Prvek1");
+            dynamickePole.PridejPrvek("Prvek2");
+            dynamickePole.PridejPrvek("Prvek3");
+            dynamickePole.PridejPrvek("Prvek4");
+            Console.WriteLine();
+
+            Console.WriteLine("Počet prvků by měl být 5.");
+            Console.WriteLine(dynamickePole.PocetPrvku);
+            Console.WriteLine();
+
+            Console.WriteLine("Vrať všechny prvky dle jejich indexů.");
+            VypisPole(dynamickePole);
+            Console.WriteLine();
+
+            Console.WriteLine("Smaž 2 prvky dle jejich indexů (1, 3).");
+            dynamickePole.VymazPrvek(1);
+            dynamickePole.VymazPrvek(3);
+            Console.WriteLine();
+
+            Console.WriteLine("Vrať všechny prvky dle jejich indexů.");
+            VypisPole(dynamickePole);
+            Console.WriteLine();
+
+            Console.WriteLine("Stiskni libovolnou klávesu pro konec.");
+            Console.ReadKey();
+        }
+
+        private static void VypisPole(DynamickePole<string> dynamickePole)
+        {
+            for (int i = 0; i < dynamickePole.PocetPrvku; i++)
+            {
+                Console.WriteLine($"Prvek na indexu {i} je {dynamickePole.VratPrvek(i)}.");
+            }
+        }
+    }
+}
